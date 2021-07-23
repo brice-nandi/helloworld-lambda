82c760e8f3f9ee1c79937088f33f263e07ac4d2b
brian <brian.rook@gmail.com>
2021-07-23 11:36:17 -0600

    initial commit

---
+++ src/main/java/com/bullyrooks/helloworldlambda/HelloworldLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.bullyrooks.helloworldlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bullyrooks/helloworldlambda/HelloworldLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullyrooks.helloworldlambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
